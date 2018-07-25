df849bd0233abdb68b1db0641c306225438adaca
André Storhaug <andr3.storhaug+code@gmail.com>
2018-07-26 00:08:10 +0200

    Add sigma lang strings for the added qtype settings

--- lang/en/qtype_sigma.php
+++ lang/en/qtype_sigma.php
@@ -39,3 +39,21 @@ $string['input_options_heading'] = 'Default input options';
 $string['input_options_heading_desc'] = 'Used when creating a new question, or adding a new input to an existing question.';
 $string['only_single_letter_variables'] = 'Only single lettered variables';
 $string['only_single_letter_variables_desc'] = 'Restrict inputs to only allow single lettered variables.';
+$string['add_times_sign'] = 'Add times sign';
+$string['add_times_sign_desc'] = 'Controls whether or not to add times sign. Note: this goes on top of STACK\'s functionality to insert stars.';
+$string['math_input_mode'] = 'Math input mode';
+$string['math_input_mode_desc'] = 'Controls which set of mathematical input buttons are to be displayed.';
+
+// Math input modes.
+$string['modesimple'] = 'Simple';
+$string['modenormal'] = 'Normal';
+$string['modeadvanced'] = 'Advanced';
+$string['modecalculus'] = 'Calculus';
+
+// Strings used on the editing form.
+$string['singlevars'] = 'Single vars';
+$string['singlevars_help'] = 'Whether or not restrict inputs to only allow single lettered variables.';
+$string['addtimessign'] = 'Add times sign';
+$string['addtimessign_help'] = 'Whether or not to add times sign. This goes on top of STACk\'s functionality to insert stars.';
+$string['mathinputmode'] = 'Math input mode';
+$string['mathinputmode_help'] = 'Controls which set of mathematical input buttons are to be displayed.';
