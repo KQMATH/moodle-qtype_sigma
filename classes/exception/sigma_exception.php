84b4dd12448c5c50673fd21e6d16c7efb51c5618
André Storhaug <andr3.storhaug+code@gmail.com>
2018-07-26 00:06:37 +0200

    Add sigma exception class

---
+++ classes/exception/sigma_exception.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Base class for all the types of exception SIGMA throws.
+ *
+ * @package    qtype
+ * @subpackage sigma
+ * @author     André Storhaug <andr3.storhaug+code@gmail.com>
+ * @copyright  2018 NTNU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+namespace qtype_sigma\exception;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Represents a SIGMA exception.
+ *
+ * @author     André Storhaug <andr3.storhaug+code@gmail.com>
+ * @copyright  2018 NTNU
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class sigma_exception extends \moodle_exception {
+    public function __construct($error) {
+        parent::__construct('exceptionmessage', 'qtype_sigma', '', $error);
+    }
+}
